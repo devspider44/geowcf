478cb1ad8864847dd75a8772bf821a21d95b8559
devSpider <lee.gutierrez44@gmail.com>
2016-03-20 19:42:32 +0800

    Add reference projects on GeoWcf.Tests

---
+++ GeoWcf.Tests/ManagerTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoWcf.Contracts;
+using GeoWcf.Data;
+using GeoWcf.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace GeoWcf.Tests
+{
+   [TestClass]
+    public class ManagerTests
+    {
+       [TestMethod]
+       public void test_zip_code_retrieval()
+       {
+           Mock<IZipCodeRepository> mockZipCodeRepository = new Mock<IZipCodeRepository>();
+
+           ZipCode zipCode = new ZipCode
+           {
+               City = "LINCOL PARK",
+               State = new State {Abbreviation = "NJ"},
+               Zip = "07035"
+           };
+
+           mockZipCodeRepository.Setup(obj => obj.GetByZip("07035")).Returns(zipCode);
+
+           IGeoService geoService = new GeoManager(mockZipCodeRepository.Object);
+
+           ZipCodeData data = geoService.GetZipinfo("07035");
+
+           Assert.IsTrue(data.City.ToUpper() == "LINCOL PARK");
+           Assert.IsTrue(data.State == "NJ");
+       }
+    }
+}
