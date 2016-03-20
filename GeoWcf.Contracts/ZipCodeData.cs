d49a0d0cff265c050cb4d3fafa7dce64b11c82cf
devSpider <lee.gutierrez44@gmail.com>
2016-03-20 17:54:47 +0800

    Created GeoManager and implemented IGeoService

--- GeoWcf.Contracts/ZipCodeData.cs
+++ GeoWcf.Contracts/ZipCodeData.cs
@@ -10,8 +10,13 @@ namespace GeoWcf.Contracts
     [DataContract]
     public class ZipCodeData
     {
+        [DataMember]
         public string City { get; set; }
+
+        [DataMember]
         public string State { get; set; }
+
+        [DataMember]
         public string ZipCode { get; set; }      
     }
 }
