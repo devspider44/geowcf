d3041f8f9003c7075f48e510f8982875de635c64
devSpider <lee.gutierrez44@gmail.com>
2016-03-20 22:52:13 +0800

    First Run - Console Host

--- GeoWcf.ConsoleHost/Program.cs
+++ GeoWcf.ConsoleHost/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
+using GeoWcf.Services;
 
 namespace GeoWcf.ConsoleHost
 {
@@ -10,6 +12,12 @@ namespace GeoWcf.ConsoleHost
     {
         static void Main(string[] args)
         {
+            ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+            hostGeoManager.Open();
+
+            Console.WriteLine("Service is now running. Press [Enter] to exit.");
+            Console.ReadLine();
+            hostGeoManager.Close();
         }
     }
 }
