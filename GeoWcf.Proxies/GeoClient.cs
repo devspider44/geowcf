4209212a29e95c0e9e9b36cf6072abbcaaf70a7a
devSpider <lee.gutierrez44@gmail.com>
2016-03-21 17:33:37 +0800

    Added GeoWcf.Proxies Project

---
+++ GeoWcf.Proxies/GeoClient.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using GeoWcf.Contracts;
+
+namespace GeoWcf.Proxies
+{
+    public class GeoClient :ClientBase<IGeoService>, IGeoService
+
+{
+    public ZipCodeData GetZipinfo(string zip)
+    {
+        return Channel.GetZipinfo(zip);
+    }
+
+    public IEnumerable<string> GetStates(bool primaryOnly)
+    {
+        return Channel.GetStates((primaryOnly));
+    }
+
+    public IEnumerable<ZipCodeData> GetZips(string state)
+    {
+        return Channel.GetZips(state);
+    }
+
+    public IEnumerable<ZipCodeData> GetZips(string zip, int range)
+    {
+        return Channel.GetZips(zip, range);
+    }
+}
+}
