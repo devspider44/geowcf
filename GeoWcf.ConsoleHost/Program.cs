d292755965b577429c93f9eb576b999f0bac894d
devSpider <lee.gutierrez44@gmail.com>
2016-03-21 15:40:44 +0800

    Hosting Module End

--- GeoWcf.ConsoleHost/Program.cs
+++ GeoWcf.ConsoleHost/Program.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 using System.Text;
 using System.Threading.Tasks;
+using GeoWcf.Contracts;
 using GeoWcf.Services;
 
 namespace GeoWcf.ConsoleHost
@@ -13,6 +15,14 @@ namespace GeoWcf.ConsoleHost
         static void Main(string[] args)
         {
             ServiceHost hostGeoManager = new ServiceHost(typeof(GeoManager));
+
+            string address = "net.tcp://localhost:8009/GeoService";
+            Binding binding = new NetTcpBinding();
+            Type contract = typeof (IGeoService);
+
+            hostGeoManager.AddServiceEndpoint(contract, binding, address);
+
+
             hostGeoManager.Open();
 
             Console.WriteLine("Service is now running. Press [Enter] to exit.");
