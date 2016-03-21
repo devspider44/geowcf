01f13fc7a287451e115d0790859b839c962d320c
devSpider <lee.gutierrez44@gmail.com>
2016-03-22 00:40:32 +0800

    Added new Project, GeoWcf.WindowsHost

---
+++ GeoWcf.WindowsHost/MessageManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoWcf.WindowsHost.Contract;
+
+namespace GeoWcf.WindowsHost
+{
+    public class MessageManager :IMessageService
+    {
+        public void ShowMessage(string message)
+        {
+            MainWindow.MainUI.Show(message);
+        }
+    }
+}
